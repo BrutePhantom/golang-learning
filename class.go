c08d37bf17f24ca3eec9e43644d1555d5a537471
Injora <injoraman@gmail.com>
2025-11-12 12:48:08 +0530

    Answered questions :)

---
+++ class.go
@@ -0,0 +1,15 @@
+// types
+
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Injora"
+	var name1 = "Injora"
+	name2 := "Injora"
+	fmt.Println(name, name1, name2)
+}
+
+//loops
